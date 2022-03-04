2b08f55ffb59c32dc1055bcdbb0c690348e7dee8
anedomansky <nedomanskya@outlook.com>
2022-03-04 19:50:04 +0100

    Initial setup

---
+++ projects/anedomansky/ngx-toast/src/lib/ngx-toast.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ToastComponent } from './components/toast/toast.component';
+
+
+
+@NgModule({
+  declarations: [
+    ToastComponent,
+  ],
+  imports: [
+  ],
+  exports: [
+    ToastComponent,
+  ]
+})
+export class NgxToastModule { }
---
+++ projects/anedomansky/ngx-toast/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-toast
+ */
+
+export * from './lib/ngx-toast.module';
+export * from './lib/components/toast/toast.component';
