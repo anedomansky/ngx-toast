fc2928c3c9235d229ba3d9a7c455e5b31b720668
anedomansky <nedomanskya@outlook.com>
2022-03-22 19:31:28 +0100

    Worked on the direct ToastComponent creation via directives.

--- projects/anedomansky/ngx-toast/src/lib/configs/toast.config.ts
+++ projects/anedomansky/ngx-toast/src/lib/configs/toast.config.ts
@@ -1,11 +1,16 @@
 import { InjectionToken } from '@angular/core';
+import { Position } from '../enums/Position';
+import { Severity } from '../enums/Severity';
 
-export interface Config {
-
+export interface ToastConfig {
+    title: string;
+    message: string;
+    severity?: Severity;
+    position?: Position;
 }
 
 export interface ToastConfigToken {
-    config: Config;
+    config: ToastConfig;
 }
 
 export const TOAST_CONFIG = new InjectionToken<ToastConfigToken>('ToastConfig');
--- projects/anedomansky/ngx-toast/src/lib/services/toast/toast.service.ts
+++ projects/anedomansky/ngx-toast/src/lib/services/toast/toast.service.ts
@@ -1,14 +1,15 @@
 import { Inject, Injectable } from '@angular/core';
-import { Config, ToastConfigToken, TOAST_CONFIG } from '../../configs/toast.config';
+import { ToastConfig, ToastConfigToken, TOAST_CONFIG } from '../../configs/toast.config';
 import { ToastDirective } from '../../directives/toast.directive';
 import { Position } from '../../enums/Position';
 import { Severity } from '../../enums/Severity';
+import { ToastComponent } from '../../components/toast/toast.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastService {
-  toastConfig: Config;
+  toastConfig: ToastConfig;
 
   constructor(
     @Inject(TOAST_CONFIG) toastToken: ToastConfigToken,
@@ -16,11 +17,50 @@ export class ToastService {
     this.toastConfig = { ...toastToken.config };
   }
 
-  create(title: string, message: string, severity: Severity, position: Position, toastDirective: ToastDirective) {
-    this.createToast();
+  private createToast(toastDirective: ToastDirective) {
+    const viewContainerRef = toastDirective.viewContainerRef;
+    const componentRef = viewContainerRef.createComponent(ToastComponent);
+    // TODO: use componentRef.instance.XYZ to initialize component's props
+    // apply this.toastConfig to the props
   }
 
-  private createToast() {
-    // TODO: implement like loadComponent()
+  private applyConfig(individualConfig: Partial<ToastConfig>) {
+    this.toastConfig = {
+      ...this.toastConfig,
+      ...individualConfig,
+    };
+  }
+
+  private createInfoToast(config: ToastConfig, toastDirective: ToastDirective) {
+    this.createToast(toastDirective);
+  }
+
+  private createSuccessToast(config: ToastConfig, toastDirective: ToastDirective) {
+    this.createToast(toastDirective);
+  }
+
+  private createWarningToast(config: ToastConfig, toastDirective: ToastDirective) {
+    this.createToast(toastDirective);
+  }
+
+  private createErrorToast(config: ToastConfig, toastDirective: ToastDirective) {
+    this.createToast(toastDirective);
+  }
+
+  create(config: ToastConfig, toastDirective: ToastDirective) {
+    switch (config.severity) {
+      case Severity.SUCCESS:
+        this.createSuccessToast(config, toastDirective);
+        break;
+      case Severity.WARNING:
+        this.createWarningToast(config, toastDirective);
+        break;
+      case Severity.ERROR:
+        this.createErrorToast(config, toastDirective);
+        break;
+      default:
+        this.createInfoToast(config, toastDirective);
+        break;
+    }
   }
 }
