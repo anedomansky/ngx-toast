7d5a853ce2cccb55bcc0238232994af6d71304d5
anedomansky <nedomanskya@outlook.com>
2022-04-15 09:54:20 +0200

    Added Jest coverage reports.

--- jest.base.config.js
+++ jest.base.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/../../setupJest.ts'],
+  coverageDirectory: '<rootDir>/../../target/ngx-toast-app/coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 90,
+      lines: 80,
+    },
+  },
 };
--- projects/anedomansky/ngx-toast/jest.config.js
+++ projects/anedomansky/ngx-toast/jest.config.js
@@ -2,4 +2,5 @@ const baseConfig = require('../../../jest.base.config');
 module.exports = {
   ...baseConfig,
   setupFilesAfterEnv: ['<rootDir>/../../../setupJest.ts'],
+  coverageDirectory: '<rootDir>/../../../target/ngx-toast/coverage/',
 };
